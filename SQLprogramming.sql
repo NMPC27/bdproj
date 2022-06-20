b1226ca790c58bcf52dbdcc2c817c8a2aa2563ec
nunocunha <nunocunha@ua.pt>
2022-06-20 18:45:30 +0100

    updates

--- SQLprogramming.sql
+++ SQLprogramming.sql
@@ -199,3 +199,11 @@
 -- DECLARE @tmp AS INT
 -- EXEC @tmp=CheckIfInWatchlist @userID=1 , @entry_id=111161
 -- SELECT @tmp
+
+
+-- CREATE PROCEDURE DeleteWatchlistByEntryIDuserID @EntryID INT, @UserID INT
+-- AS
+-- 	DELETE FROM Watchlist WHERE username_ID_tb_watchlist=@UserID AND entry_ID_tb_watchlist=@EntryID
+-- GO
+
+-- EXEC DeleteWatchlistByEntryIDuserID @EntryID=111161, @UserID=1
