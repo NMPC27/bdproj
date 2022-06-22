84246c35bc3eb005dd164e27b6d2cd669cd3ff5f
nunocunha <nunocunha@ua.pt>
2022-06-22 10:41:54 +0100

    update final espqro eu

--- sql-p/udf-ShowInfoTeamByMenbroId.sql
+++ sql-p/udf-ShowInfoTeamByMenbroId.sql
@@ -1,6 +1,6 @@
-CREATE FUNCTION ShowInfoTeamByMenbroId(@actor_id INT) RETURNS Table
+CREATE FUNCTION ShowInfoTeamByMembroId(@actor_id INT) RETURNS Table
 AS
     RETURN (SELECT * FROM Pessoa JOIN Team_Member ON Pessoa.ID=Team_Member.tb_membroID WHERE Team_Member.tb_membroID=@actor_id)
 GO
 
--- SELECT * FROM ShowInfoTeamByMenbroId(5);
+-- SELECT * FROM ShowInfoTeamByMembroId(5);
--- sql-p/udf-ShowWhereCastByMenberId.sql
+++ sql-p/udf-ShowWhereCastByMenberId.sql
@@ -1,6 +1,6 @@
-CREATE FUNCTION ShowWhereCastByMenberId(@actor_id INT) RETURNS Table
+CREATE FUNCTION ShowWhereCastByMemberId(@actor_id INT) RETURNS Table
 AS
     RETURN (SELECT * FROM Entry_Team JOIN Media_Entry ON Entry_Team.entryID_tb_team=Media_Entry.entry_ID WHERE Entry_Team.membroID=@actor_id)
 GO
 
---SELECT * FROM ShowWhereCastByMenberId(5);
+--SELECT * FROM ShowWhereCastByMemberId(5);
